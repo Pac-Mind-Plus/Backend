2f99478f12647cf352c7808c68ef03bc2b70ddd5
Arthur Hoengen <arthurhoengen@gmail.com>
2024-07-09 19:35:32 -0300

    Adicionando CrossOrigin

--- src/main/java/catolica/mindplus/mindplus/controllers/ActionGroupsController.java
+++ src/main/java/catolica/mindplus/mindplus/controllers/ActionGroupsController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(path = "/actiongroups")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ActionGroupsController {
     @Autowired
     ActionsGroupsService actionsGroupsService;
