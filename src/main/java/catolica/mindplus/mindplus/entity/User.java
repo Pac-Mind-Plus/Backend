11cd4e663717495e4b621dc8bdc45aa4172a945e
ArthurHoengen <arthurhoengen@gmail.com>
2024-12-02 22:08:06 -0300

    Arrumando user

--- src/main/java/catolica/mindplus/mindplus/entity/User.java
+++ src/main/java/catolica/mindplus/mindplus/entity/User.java
@@ -6,20 +6,28 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "person")
+@Table(name = "person", uniqueConstraints = @UniqueConstraint(columnNames = { "person_user_id" }))
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
+    @Column(name = "user_id")
+    private String userId;
+
     @Column(name = "username")
     private String username;
 
     @Column(name = "name")
     private String name;
+
+    public String getUserId() {
+        return userId;
+    }
 
     @Column(name = "lastName")
     private String lastName;
